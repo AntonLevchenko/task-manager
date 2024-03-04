9ca7c9e93cb0834aa88566cc68d061771af8745a
devwce <dev.wce@gmail.com>
2024-03-04 13:04:44 +0200

    fixed displaying assigned user

--- src/app/modules/tasks/components/assigned-user/assigned-user.component.ts
+++ src/app/modules/tasks/components/assigned-user/assigned-user.component.ts
@@ -13,9 +13,10 @@ export class AssignedUserComponent implements OnInit {
   @Input() assignedUserId: string | null;
   @Output() onAssignUserId: EventEmitter<string> = new EventEmitter();
 
-  public control = new FormControl('');
+  public control: FormControl<User> = new FormControl();
   public options: User[] = [];
   public filteredOptions: Observable<User[]>;
+  public unselectedUserData: User = {} as User;
 
   constructor(
     private usersService: UsersService
@@ -23,12 +24,11 @@ export class AssignedUserComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    if (this.assignedUserId) {
-      this.control.setValue(this.assignedUserId);
-    }
+    this.assignUser();
+
     this.filteredOptions = this.control.valueChanges.pipe(
-      startWith(''),
-      map(value => this.filter(value || '')),
+      startWith(this.unselectedUserData),
+      map(value => this.filter(value)),
     );
 
     this.usersService.data$.pipe(
@@ -39,12 +39,26 @@ export class AssignedUserComponent implements OnInit {
   }
 
   public onSelectionChange(event: any): void {
-    this.onAssignUserId.emit(event.option.value);
+    this.onAssignUserId.emit(event.option.value.id);
   }
 
-  private filter(value: string): User[] {
-    const filterValue = value.toLowerCase();
+  private filter(value: User): User[] {
+    const filterValue = value.name?.toLowerCase() ?? '';
 
     return this.options.filter((item: User) => item.name.toLowerCase().includes(filterValue));
+  }
+
+  public displayFn(user: User): string {
+    return user?.name ?? '';
+  }
+
+  private assignUser() {
+    if (this.assignedUserId) {
+      this.usersService.getById(this.assignedUserId).pipe(
+        take(1)
+      ).subscribe((user: User) => {
+        this.control.setValue(user);
+      })
+    }
   }
 }
